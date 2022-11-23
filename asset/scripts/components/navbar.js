baa77eade467060cd54fdf67e694eaac7b21ed60
chanayus <chanayus1526@hotmail.com>
2022-11-23 18:57:55 +0700

    fix css browser support

--- asset/scripts/components/navbar.js
+++ asset/scripts/components/navbar.js
@@ -11,9 +11,9 @@ class Navbar extends HTMLElement {
                 <img src="./asset/images/logo.svg" alt="logo" width="90" />
             </a>
 
-            <div class="flex md:gap-4 gap-3">
+            <div class="flex">
                 <a href=""><img src="./asset/icons/user.svg" alt="user-icon" /></a>
-                <a href=""><img src="./asset/icons/heart.svg" alt="heart-icon" /></a>
+                <a href=""><img src="./asset/icons/heart.svg" alt="heart-icon" class="md:mx-4 mx-3" /></a>
                 <a href=""><img src="./asset/icons/bag.svg" alt="bag-icon" /></a>
             </div>
         
